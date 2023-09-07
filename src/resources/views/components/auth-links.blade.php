2032bca0c771987e1c5a841ef7abb533ae8b900e
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-07 22:43:14 +0930

    Updates

--- src/resources/views/components/auth-links.blade.php
+++ src/resources/views/components/auth-links.blade.php
@@ -3,7 +3,7 @@
 
     <a href="{{ route('profile.edit') ?? '#' }}">{{ __('Profile') }}</a>
 
-    <form method="POST" action="{{ route('logout') ?? '#' }}">
+    <form method="POST" action="{{ route('logout') ?? '#' }}" style="display: inline;">
         @csrf
         <a href="{{ route('logout') ?? '#' }}" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</a>
     </form>
