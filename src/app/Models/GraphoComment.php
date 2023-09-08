224c9423c0a17cbe850365937d5d01c2c8dcfebb
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-08 23:36:53 +0930

    Comments

--- src/app/Models/GraphoComment.php
+++ src/app/Models/GraphoComment.php
@@ -4,7 +4,8 @@ namespace AMoschou\Grapho\App\Models;
  
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
- 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class GraphoComment extends Model
 {
     /**
