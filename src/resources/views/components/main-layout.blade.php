0fc833bd7316914990f79602d6e3f21f60e913b8
amoschou <4948673+amoschou@users.noreply.github.com>
2023-10-06 15:12:27 +1030

    Update main-layout.blade.php

--- src/resources/views/components/main-layout.blade.php
+++ src/resources/views/components/main-layout.blade.php
@@ -44,6 +44,8 @@
                 border-left: 0.25em solid var(--color-border-default);
             }
 
+            .markdown-body div.md-alert.md-alert-note > span { font-weight: var(--base-text-weight-medium, 500) !important; }
+
             .markdown-body div.md-alert.md-alert-note { border-left-color: #0969DA; }
             .markdown-body div.md-alert.md-alert-important { border-left-color: #8250df; }
             .markdown-body div.md-alert.md-alert-warning { border-left-color: #9A6700; }
