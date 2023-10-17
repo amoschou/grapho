1e0f3ea5bce51ffd6c7c171c15ced213504c68fc
amoschou <4948673+amoschou@users.noreply.github.com>
2023-10-17 16:01:58 +1030

    Updates

--- src/resources/views/pdf/table-of-contents.blade.php
+++ src/resources/views/pdf/table-of-contents.blade.php
@@ -16,6 +16,35 @@
     :online="$online"
 >
 
+<style>
+    ol {
+        counter-reset: section; /* Creates a new instance of the
+                                    section counter with each ol
+                                    element */
+        list-style-type: none;
+    }
+
+    li::before {
+        counter-increment: section; /* Increments only this instance
+                                                    of the section counter */
+        content: counters(section, ".") " "; /* Combines the values of all instances
+                                                of the section counter, separated
+                                                by a period */
+    }
+
+    .markdown-body ol {
+        list-style-type: upper-roman;
+    }
+
+    .markdown-body ol ol,
+    .markdown-body ol ol ol,
+    .markdown-body ol ol ol ol,
+    .markdown-body ol ol ol ol ol
+    {
+        list-style-type: numeric;
+    }
+</style>
+
 <h2>Table of contents</h2>
 
 <?php
