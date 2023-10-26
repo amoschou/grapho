7caf002d4fe044cfd2d6203c5e94be1f1100ec2c
amoschou <4948673+amoschou@users.noreply.github.com>
2023-10-26 17:00:13 +1030

    Updates

--- src/resources/views/components/toc.blade.php
+++ src/resources/views/components/toc.blade.php
@@ -1,5 +1,5 @@
 @php
-    $tocNode = AMoschou\Grapho\App\Http\Controllers\GraphoController::tableOfContents();
+    $tocNode = AMoschou\Grapho\App\Http\Controllers\GraphoController::tableOfContents2();
 @endphp
 
 <x-grapho::toc-content-items :node="$tocNode" />
