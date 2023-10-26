acf1fa64e7bbebd465f7da3e40247c7844b1a597
amoschou <4948673+amoschou@users.noreply.github.com>
2023-10-26 16:41:47 +1030

    Updates

--- src/app/Classes/DocFile2.php
+++ src/app/Classes/DocFile2.php
@@ -2,19 +2,19 @@
 
 namespace AMoschou\Grapho\App\Classes;
 
-use AMoschou\Grapho\App\Classes\DocFile as LegacyDocFile;
+// use AMoschou\Grapho\App\Classes\DocFile as LegacyDocFile;
 use SplFileInfo;
+use League\CommonMark\Extension\FrontMatter\Data\SymfonyYamlFrontMatterParser;
+use League\CommonMark\Extension\FrontMatter\FrontMatterParser;
+use Throwable;
 
 class DocFile2
 {
     public string $sourcePath;
-
     public string $relativePath;
-
     public string $fullPath;
-
-    public LegacyDocFile $legacyDocFile;
-
+    private $defaultOrd = 0;
+    // public LegacyDocFile $legacyDocFile;
     private SplFileInfo $splFileInfo;
 
     public function __construct($relativePath)
@@ -27,6 +27,29 @@ class DocFile2
 
         $this->splFileInfo = new SplFileInfo($this->fullPath);
 
-        $this->legacyDocFile = new LegacyDocFile($this->fullPath);
+        // $this->legacyDocFile = new LegacyDocFile($this->fullPath);
+
+        $this->constructMetadata();
+    }
+
+    public function getOrd() { return $this->getMetadata('ord'); }
+    private function getMetadata($key) { return $this->metadata[$key]; }
+    private function constructMetadata()
+    {
+        $default = [
+            'ord' => $this->defaultOrd,
+            'title' => $this->splFileInfo->getBaseName('.md'),
+        ];
+
+        try {
+            $this->metadata = array_merge(
+                $default,
+                (new FrontMatterParser(new SymfonyYamlFrontMatterParser()))
+                    ->parse(file_get_contents($this->splFileInfo->getRealPath()))
+                    ->getFrontMatter()
+            );
+        } catch (Throwable) {
+            $this->metadata = $default;
+        }
     }
 }
